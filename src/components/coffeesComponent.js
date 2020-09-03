439e419009c6dd6bce021651293bfe0717c7be2a
unknown <qng1997@gmail.com>
2020-09-03 11:59:44 -0700

    first commit

---
+++ src/components/coffeesComponent.js
@@ -0,0 +1,19 @@
+import React from "react"
+import SingularComp from "./singularComp"
+
+const coffeesComponent = () => {
+  return (
+    <div>
+      <SingularComp name="Bistro"></SingularComp>
+      <SingularComp name="Bistro"></SingularComp>
+
+      <SingularComp name="Bistro"></SingularComp>
+
+      <SingularComp name="Bistro"></SingularComp>
+
+      <SingularComp name="Bistro"></SingularComp>
+    </div>
+  )
+}
+
+export default coffeesComponent
